262b16b9bd7a6267d9c5ab9bf37a19cdb0b03c83
Tsiaputa Mykhailo <mykhailo.tsiaputa.ki.2022@lpnu.ua>
2025-11-13 14:30:56 +0200

    Cleanup: Remove build directory from version control

--- build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Admin/csad_2025_KI407_TsiaputaMykhailoPetrovych_24/build/_deps/googletest-src
-# Build directory: C:/Users/Admin/csad_2025_KI407_TsiaputaMykhailoPetrovych_24/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Admin/csad_2025_KI407_TsiaputaMykhailoPetrovych_24/build/_deps/googletest-src/googletest
-# Build directory: C:/Users/Admin/csad_2025_KI407_TsiaputaMykhailoPetrovych_24/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
